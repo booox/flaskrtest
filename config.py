fce98c86de967b06debf94c3a9416d5727518efd
booox <booox@users.noreply.github.com>
2016-04-04 20:53:59 +0800

    after clone first commit

---
+++ config.py
@@ -0,0 +1,56 @@
+﻿#coding=utf-8
+
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Can you guess it!'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    
+    BOOTSTRAP_SERVE_LOCAL = True
+    
+    MAIL_SERVER = 'smtp.163.com'
+    MAIL_PORT = 25
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') 
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') # 163 auth code
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = os.environ.get('MAIL_USERNAME')
+    
+    FLASK_ADMIN = os.environ.get('MAIL_USERNAME')  # just for test
+    FLASK_POSTS_PER_PAGE = 20  # pagination
+    
+    @staticmethod
+    def init_app(app):
+        pass
+        
+    
+class DevelopmentConfig(Config):
+    DEBUG = True       
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+            'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+            
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+            'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+            
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+            'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+            
+config = {
+    'development' : DevelopmentConfig,
+    'testing' : TestingConfig,
+    'production' : ProductionConfig,
+    'default' : DevelopmentConfig
+}
+            
+            
+            
+            
+            
